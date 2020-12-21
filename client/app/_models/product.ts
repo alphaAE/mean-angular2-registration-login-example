b2b4f104337c03c09d93d2585ca4a3c97e2b7a40
alphaae <a1226123914@gmail.com>
2020-12-21 13:02:48 +0800

    前端怎加商品server

---
+++ client/app/_models/product.ts
@@ -0,0 +1,5 @@
+﻿export class Product {
+    _id: string;
+    name: string;
+    price: string;
+}
--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,7 +1,8 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, Input } from '@angular/core';
 
 import { User } from '../_models/index';
-import { UserService } from '../_services/index';
+import { Product } from '../_models/product';
+import { UserService, ProductService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -9,22 +10,39 @@ import { UserService } from '../_services/index';
 })
 
 export class HomeComponent implements OnInit {
+
+    @Input() createProduct: Product;
+
     currentUser: User;
     users: User[] = [];
 
-    constructor(private userService: UserService) {
+    products: Product[] = [];
+
+    constructor(private productService: ProductService, private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
         this.loadAllUsers();
+        this.loadAllProducts();
     }
+
 
     deleteUser(_id: string) {
         this.userService.delete(_id).subscribe(() => { this.loadAllUsers() });
     }
 
+    deleteProduct(_id: string) {
+        this.productService.delete(_id).subscribe(() => { this.loadAllProducts() })
+    }
+
     private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+    private loadAllProducts() {
+        this.productService.getAll().subscribe(products => { 
+            this.products = products;
+        })
     }
 }
